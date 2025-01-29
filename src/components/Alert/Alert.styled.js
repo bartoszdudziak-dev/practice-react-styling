ae4c151cf8a7e9da34cff3311e02284e23a96ab2
Bartosz Dudziak <bartoszdudziak.dev@gmail.com>
2025-01-29 19:52:32 +0100

    Task 01

--- src/components/Alert/Alert.styled.js
+++ src/components/Alert/Alert.styled.js
@@ -1,7 +1,29 @@
 import styled from 'styled-components';
+import defaultTheme from "../../themes/defaultTheme";
 
 const StyledAlert = styled.div`
-    display: block;
-`
+  display: block;
+  padding: 0.75rem 1.25rem;
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 5px;
+
+  ${({ theme, variant }) => {
+    const alertTheme = { ...defaultTheme, ...theme };
+    const { color, borderColor, backgroundColor } = alertTheme[variant];
+
+    return `
+      color: ${color};
+      background-color: ${backgroundColor};
+      border-color: ${borderColor};
+    `;
+  }}
+`;
+
+StyledAlert.defaultProps = {
+  variant: "primary",
+  theme: defaultTheme,
+};
+
 
 export { StyledAlert };
